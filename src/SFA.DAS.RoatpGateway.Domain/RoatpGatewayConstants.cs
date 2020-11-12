10048f7c5e0754689ec2b256314f635642a80c65
gdbarnes <gregdbarnes@gmail.com>
2020-11-12 14:43:13 +0000

    Apsotrophe change and add new caption commented out in RoatpGatewayConstants.cs

--- src/SFA.DAS.RoatpGateway.Domain/RoatpGatewayConstants.cs
+++ src/SFA.DAS.RoatpGateway.Domain/RoatpGatewayConstants.cs
@@ -10,10 +10,13 @@ namespace SFA.DAS.RoatpGateway.Domain
         public static class Captions
         {
             public static string OrganisationChecks = "Organisation checks";
-
             public static string PeopleInControlChecks = "People in control checks";
             public static string ExperienceAndAccreditation = "Experience and accreditation checks";
-            public static string OrganisationsCriminalAndComplianceChecks = "Organisation’s criminal and compliance checks";
+            public static string OrganisationsCriminalAndComplianceChecks = "Organisation's criminal and compliance checks";
+
+            // This one needs adding Mark, currently both sections 5 and 6 share the one above - Greg
+            // public static string PeopleInControlCriminalAndComplianceChecks = "People in control's criminal and compliance checks";
+
             public static string RegisterChecks = "Register checks";
         }
 
