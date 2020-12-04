f3007680d1611327bcc30393dd5afce7eb5c812a
Martin Lush <martin-hornsby@ntlworld.com>
2020-12-04 11:42:06 +0000

    APR-2016 - Create helper class to deal with common styling logic

---
+++ src/SFA.DAS.RoatpGateway.Web/Helpers/CssStylingHelper.cs
@@ -0,0 +1,34 @@
+﻿using SFA.DAS.RoatpGateway.Domain;
+
+namespace SFA.DAS.RoatpGateway.Web.Helpers
+{
+    public static class CssStylingHelper
+    {
+        public static string GetSectionReviewStatusStyle(string status)
+        {
+            string cssModifierClassName;
+
+            switch (status)
+            {
+                case SectionReviewStatus.NotRequired:
+                    cssModifierClassName = "govuk-tag--inactive";
+                    break;
+                case SectionReviewStatus.Pass:
+                    cssModifierClassName = "das-tag--solid-green";
+                    break;
+                case SectionReviewStatus.Fail:
+                    cssModifierClassName = "das-tag--solid-red";
+                    break;
+                //case SectionReviewStatus.Clarification:
+                //    cssModifierClassName = "das-tag--solid-purple";
+                //    break;
+                case SectionReviewStatus.InProgress:
+                default:
+                    cssModifierClassName = "";
+                    break;
+            }
+
+            return cssModifierClassName;
+        }
+    }
+}
