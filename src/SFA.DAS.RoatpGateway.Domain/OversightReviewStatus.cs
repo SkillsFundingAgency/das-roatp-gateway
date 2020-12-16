5eb9b4e9edbb977a118623fd7fe46066d0d0356e
Martin Lush <martin-hornsby@ntlworld.com>
2020-12-16 14:03:12 +0000

    APR-2032 - Include Oversight Review status in viewmodel

---
+++ src/SFA.DAS.RoatpGateway.Domain/OversightReviewStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.RoatpGateway.Domain
+{
+    public static class OversightReviewStatus
+    {
+        public const string New = "New";
+        public const string Successful = "Successful";
+        public const string Unsuccessful = "Unsuccessful";
+    }
+}
