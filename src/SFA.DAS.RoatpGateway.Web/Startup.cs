57ddf1f7ae9be172665ab3ab5ca5d0068dbc78d4
Deven Shah <devenshah1974@gmail.com>
2026-01-09 13:46:37 +0000

    CSP-2394 Fixed configuration resolution issue

--- src/SFA.DAS.RoatpGateway.Web/Startup.cs
+++ src/SFA.DAS.RoatpGateway.Web/Startup.cs
@@ -62,7 +62,7 @@ public class Startup
         );
 
         _configuration = config.Build();
-        ApplicationConfiguration = _configuration.GetSection(nameof(WebConfiguration)).Get<WebConfiguration>();
+        ApplicationConfiguration = _configuration.Get<WebConfiguration>();
     }
 
     // This method gets called by the runtime. Use this method to add services to the container.
